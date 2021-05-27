e9e65ebdba97c3a33724a6eb6974e210ee3bfd8e
yuki9326 <ukey.0613@gmail.com>
2021-05-27 06:24:27 +0000

    [Assignment]3-8

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,46 @@
+class BooksController < ApplicationController
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def create
+    @books = Book.all
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "successfully"
+      redirect_to book_path(@book.id)
+    else flash.now[:danger] = "error"
+      render:index
+    end
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book =Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+    flash[:notice] = "successfully"
+    redirect_to book_path(@book.id)
+    else flash.now[:danger] = "error"
+      render:edit
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_index_path
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title,:body)
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+get 'books' => 'books#index',as:'books'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+post'books' => 'books#create'
+get 'books/:id' => 'books#show', as:'book'
+get 'books/:id/edit' => 'books#edit', as:'edit_book'
+patch 'books/:id' => 'books#update', as:'update_book'
+delete 'books/:id' => 'books#destroy', as:'destroy_book'
+root to: 'homes#top'
+end
